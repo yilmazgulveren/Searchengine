955014539900b06faa9e63d2851d0f5e9d74c92a
yilmazgulveren <37828628+yilmazgulveren@users.noreply.github.com>
2019-02-05 18:12:15 +0200

    Add files via upload

---
+++ WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Search = new System.Windows.Forms.Button();
+            this.Browse = new System.Windows.Forms.Button();
+            this.Text = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.IbxResults = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // Search
+            // 
+            this.Search.Location = new System.Drawing.Point(526, 51);
+            this.Search.Name = "Search";
+            this.Search.Size = new System.Drawing.Size(75, 23);
+            this.Search.TabIndex = 0;
+            this.Search.Text = "Search";
+            this.Search.UseVisualStyleBackColor = true;
+            this.Search.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Browse
+            // 
+            this.Browse.Location = new System.Drawing.Point(526, 93);
+            this.Browse.Name = "Browse";
+            this.Browse.Size = new System.Drawing.Size(75, 23);
+            this.Browse.TabIndex = 1;
+            this.Browse.Text = "Browse";
+            this.Browse.UseVisualStyleBackColor = true;
+            this.Browse.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Text
+            // 
+            this.Text.Location = new System.Drawing.Point(215, 51);
+            this.Text.Name = "Text";
+            this.Text.Size = new System.Drawing.Size(294, 22);
+            this.Text.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(69, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(140, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Filename Containing:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(215, 93);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(294, 22);
+            this.textBox2.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(69, 96);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Directory:";
+            // 
+            // IbxResults
+            // 
+            this.IbxResults.FormattingEnabled = true;
+            this.IbxResults.ItemHeight = 16;
+            this.IbxResults.Location = new System.Drawing.Point(215, 139);
+            this.IbxResults.Name = "IbxResults";
+            this.IbxResults.Size = new System.Drawing.Size(294, 276);
+            this.IbxResults.TabIndex = 6;
+            this.IbxResults.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(672, 450);
+            this.Controls.Add(this.IbxResults);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Text);
+            this.Controls.Add(this.Browse);
+            this.Controls.Add(this.Search);
+            this.Name = "Form1";
+            this.Text = "Search Engine";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Search;
+        private System.Windows.Forms.Button Browse;
+        private System.Windows.Forms.TextBox Text;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox IbxResults;
+    }
+}
+
---
+++ WindowsFormsApp3/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        string strSearchDirectory;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+
+            if (fbd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                strSearchDirectory = fbd.SelectedPath;
+                txtSearchDirectory.Text = strSearchDirectory;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            strSearchDirectory = txtSearchDirectory.Text;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DirectoryInfo di = new DirectoryInfo(strSearchDirectory);
+            FileInfo[] fi = di.GetFiles();
+
+            if (txtSearchSubstring.Text != "")
+            {
+                string strSearchString = txtSearchSubstring.Text;
+                IbxResults.Items.Clear();
+                foreach (FileInfo curfile in fi)
+                {
+                    if (curfile.Name.ToUpper().IndexOf(strSearchString.ToUpper()) != -1)
+                    {
+                        IbxResults.Items.Add(curfile.Name);
+                    }//if
+                }//foreach
+                txtSearchSubstring.Clear();
+                txtSearchSubstring.Focus();
+            }//if
+        }//buttonclick
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
